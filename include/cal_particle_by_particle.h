f9c331622981e9f6c6146440e11dbf0edc6bd260
Liuqi19990113 <78146233+Liuqi19990113@users.noreply.github.com>
2021-04-05 21:50:19 +0800

    update cal_particle_by_particle.h

--- include/cal_particle_by_particle.h
+++ include/cal_particle_by_particle.h
@@ -7,7 +7,7 @@
 using namespace std;
 
 int sample_eta_particle_by_particle(double qgp_energy,double qgp_pz, double accept_error, string oscar_path);
-int cal_eta_particle_by_particle(double qgp_energy,double qgp_pz, double accept_error,map<double,double>eta_energy_map,map<double,double>eta_pz_map,vector<double>eta_list);;
+int cal_eta_particle_by_particle(double qgp_energy,double qgp_pz, double accept_error,map<double,double>eta_energy_map,map<double,double>eta_pz_map,vector<double>eta_list,ofstream &eta_cut_output_file);;
 
 
 
